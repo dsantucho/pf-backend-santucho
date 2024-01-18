2b32763cbdb74d067c58ae1abd8248df752ae20a
dsantucho <soledadsantucho@hotmail.com>
2024-01-18 11:58:56 -0300

    limpio comentarios

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-//import routerProd from "./routes/product.routes.js";
 // -------- Servidor Express ---------
 const express = require("express");
 const routerProd = require("./routes/product.routes.js");
@@ -19,11 +18,3 @@ app.use('/api/carts/', routerCart)
 app.listen(PORT, () => {
     console.log("Puerto arriba en consola: ", PORT);
 })
-//----- Tests -----
-/* 
-Test 0 - http://localhost:8080/ Server Express arriba mensaje
-Test1 -  http://localhost:8080/products sin query, eso debe devolver todos los 10 productos.
-Test2 -  http://localhost:8080/products?limit=5 , eso debe devolver sólo los primeros 5 de los 10 productos.
-Test3 -  http://localhost:8080/products/2, eso debe devolver sólo el producto con id=2.
-Test4 -  http://localhost:8080/products/34123123, al no existir el id del producto, debe devolver un objeto con un error indicando que el producto no existe.
-*/
