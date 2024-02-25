8b8cf54a4504ef735a63b7eff5228e5dde6e6283
dsantucho <soledadsantucho@hotmail.com>
2024-02-25 19:57:06 -0300

    view peducts con paginacion

--- src/public/js/index.js
+++ src/public/js/index.js
@@ -1,3 +1,12 @@
+let paginationLinks = [];  // Array para almacenar los objetos de paginación
+function pagesInfo(data){
+  document.getElementById('pages').innerHTML = (`
+    <div>
+      <h3>Total Pages = ${data.totalPages}</h3>
+      <h4>Current Page = ${data.currentPage}</h4>
+    </div>
+  `);
+};
 function render(data) {
   const html = data.map(elem => {
     return (`
@@ -14,13 +23,67 @@ function render(data) {
         `)
   }).join(' ')
 
-  document.getElementById('productList').innerHTML = html
+  document.getElementById('productList').innerHTML = html;
 };
+
+function paginationData(paginationLinks) {
+  const html = paginationLinks.map(linkObj => {
+    if (linkObj.type === 'prevLink') {
+      return (`
+        <div>
+          <button onclick="handlePageClick('${linkObj.link}')" class="bg-blue-500 hover:bg-blue-700 text-black font-bold py-2 px-4 mr-2 rounded" ${linkObj.link ? '' : 'disabled'}>
+            Prev Page
+          </button>
+        </div>
+      `);
+    } else if (linkObj.type === 'nextLink') {
+      return (`
+        <div>
+          <button onclick="handlePageClick('${linkObj.link}')" class="bg-blue-500 hover:bg-blue-700 text-black font-bold py-2 px-4 ml-2 rounded" ${linkObj.link ? '' : 'disabled'}>
+            Next Page
+          </button>
+        </div>
+      `);
+    }
+  }).join(' ');
+
+  document.getElementById('pagination').innerHTML = html;
+}
+function handlePageClick(link) {
+  if (link && link !== null) {
+    fetch(link).then(response => response.json()).then(data => {
+      updatePagination(data);
+      pagesInfo(data);
+      render(data.payload);
+    });
+  }
+}
+
+function updatePagination(data) {
+  paginationLinks = [];  // Limpiar el array antes de actualizarlo
+
+  if (data.prevLink) {
+    paginationLinks.push({ type: 'prevLink', link: data.prevLink });
+  }
+
+  if (data.nextLink) {
+    paginationLinks.push({ type: 'nextLink', link: data.nextLink });
+  }
+
+  console.log(paginationLinks);
+  paginationData(paginationLinks);
+}
+
 //por HTTP request
-fetch('http://localhost:8080/api/products/').then((response) => {
+fetch('http://localhost:8080/api/products/?limit=4&page=1').then((response) => {
   response.json().then(data => {
-    render(data);
+    render(data.payload);
+    updatePagination(data);
+    pagesInfo(data)
+    console.log(data)
   });
   console.log('Se envio la data desde fetch');
 });
+
+
 
--- src/routes/app.routes.js
+++ src/routes/app.routes.js
@@ -4,9 +4,9 @@ const {Router} = express
 const router = new Router()
 
 
-router.get('/home', async (req, res)=>{
+router.get('/products', async (req, res)=>{
   // Renderizar la vista home.handlebars y pasarle la lista de productos
-  res.render('home',{});
+  res.render('products',{});
 }) 
 
 router.get('/realtimeProducts', async (req, res)=>{
